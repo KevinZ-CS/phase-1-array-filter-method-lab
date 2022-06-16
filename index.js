8635f7867171bab881e1b4a5794f741f3a0be86f
Kevin Zheng <kevzheng27@gmail.com>
2022-06-15 20:24:09 -0400

    commit tree d7422648569a0098d3654391bfb25cd1e677c571

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const newList = drivers.filter(eachDriver => name.toUpperCase() === eachDriver.toUpperCase());
+    return newList;
+}
+
+function fuzzyMatch(drivers, name) {
+    const newList = drivers.filter(eachDriver => eachDriver.substr(0, 2).includes(name));
+    return newList;
+}
+
+function matchName(drivers, driverName) {
+    const newList = drivers.filter(eachDriver => eachDriver.name === driverName);
+    return newList;
+}
